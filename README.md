8089bf601397981a93d9795bc13329c2ce395804
Danilo Guanabara <danilo@sensorial.systems>
2025-03-10 18:58:38 -0300

    :sparkles: dx as a binary dependency

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ Rust crate for creating WebComponents
 
 # Installation
 
+## General requirements
+
+- Cargo 1.84.1
+- Rust 1.84.1
+- cargo install dioxus-cli --version 0.6.3
+
+## Install cargo-web-component
+
 ```bash
 cargo install cargo-web-component --git https://github.com/sensorial-systems/web-component
 ```
--- crates/cargo-web-component/src/main.rs
+++ crates/cargo-web-component/src/main.rs
@@ -1,23 +1,14 @@
 #![doc = include_str!("../README.md")]
 
-use dioxus_cli::{StructuredOutput, link, Result};
 use clap::Parser;
+use anyhow::{Result, Context};
 
 #[derive(Parser)]
 #[command(name = "cargo-web-component", bin_name = "cargo web-component")]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[command(subcommand)]
-    command: Commands,
-
-    #[command(flatten)]
-    pub verbosity: dioxus_cli::Verbosity,
-}
-
-#[derive(Parser)]
-struct WebComponentArgs {
-    #[command(subcommand)]
-    command: AllCommands,
+    command: Commands
 }
 
 #[derive(Parser)]
@@ -26,68 +17,16 @@ struct NewArgs {
 }
 
 #[derive(Parser)]
-enum AllCommands {
+enum Commands {
     /// Create a new project for Dioxus.
     #[clap(name = "new")]
     New(NewArgs),
-    
-    /// Build the Dioxus project and all of its assets.
-    #[clap(name = "build")]
-    Build(dioxus_cli::BuildArgs),
-
-    /// Translate a source file into Dioxus code.
-    #[clap(name = "translate")]
-    Translate(dioxus_cli::Translate),
-
-    /// Build, watch & serve the Dioxus project and all of its assets.
+    /// Serve the project.
     #[clap(name = "serve")]
-    Serve(dioxus_cli::ServeArgs),
-
-    /// Init a new project for Dioxus in the current directory (by default).
-    /// Will attempt to keep your project in a good state.
-    #[clap(name = "init")]
-    Init(dioxus_cli::Init),
-
-    /// Clean output artifacts.
-    #[clap(name = "clean")]
-    Clean(dioxus_cli::Clean),
-
-    /// Bundle the Dioxus app into a shippable object.
-    #[clap(name = "bundle")]
-    Bundle(dioxus_cli::Bundle),
-
-    /// Automatically format RSX.
-    #[clap(name = "fmt")]
-    Autoformat(dioxus_cli::Autoformat),
-
-    /// Check the project for any issues.
-    #[clap(name = "check")]
-    Check(dioxus_cli::Check),
-
-    /// Run the project without any hotreloading
-    #[clap(name = "run")]
-    Run(dioxus_cli::RunArgs),
-
-    /// Ensure all the tooling is installed and configured correctly
-    #[clap(name = "doctor")]
-    Doctor(dioxus_cli::Doctor),
-
-    /// Dioxus config file controls.
-    #[clap(subcommand)]
-    #[clap(name = "config")]
-    Config(dioxus_cli::Config),
+    Serve,
 }
 
-
-#[derive(Parser)]
-enum Commands {
-    #[clap(hide = true)]
-    WebComponent(WebComponentArgs),
-    #[clap(flatten)]
-    AllCommands(AllCommands),
-}
-
-async fn process_new_command(opts: NewArgs) -> Result<StructuredOutput> {
+async fn process_new_command(opts: NewArgs) -> Result<()> {
     let path = std::path::Path::new(&opts.name);
     std::fs::create_dir_all(path.join("assets")).unwrap();
     std::fs::create_dir_all(path.join("src/logo")).unwrap();
@@ -102,59 +41,39 @@ async fn process_new_command(opts: NewArgs) -> Result<StructuredOutput> {
     std::fs::write(path.join("src/navigation_bar/mod.rs"), include_str!("templates/project/src/navigation_bar/mod.rs")).unwrap();
     std::fs::write(path.join("src/navigation_bar/style.css"), include_str!("templates/project/src/navigation_bar/style.css")).unwrap();
     std::fs::write(path.join("src/parameterized_route/mod.rs"), include_str!("templates/project/src/parameterized_route/mod.rs")).unwrap();
-    Ok(StructuredOutput::Success)
+    Ok(())
 }
 
-async fn process_all_commands(opts: AllCommands) -> Result<(), Box<dyn std::error::Error>> {
-    let result = match opts {
-        AllCommands::New(opts) => process_new_command(opts).await,
-        AllCommands::Translate(opts) => opts.translate(),
-        AllCommands::Init(opts) => opts.init(),
-        AllCommands::Config(opts) => opts.config(),
-        AllCommands::Autoformat(opts) => opts.autoformat(),
-        AllCommands::Check(opts) => opts.check().await,
-        AllCommands::Clean(opts) => opts.clean().await,
-        AllCommands::Build(opts) => opts.run_cmd().await,
-        AllCommands::Serve(opts) => opts.serve().await,
-        AllCommands::Bundle(opts) => opts.bundle().await,
-        AllCommands::Run(opts) => opts.run().await,
-        AllCommands::Doctor(opts) => opts.run().await,    
-    };
-
-    // Provide a structured output for third party tools that can consume the output of the CLI
-    match result {
-        Ok(output) => {
-            tracing::debug!(json = ?output);
-        }
-        Err(err) => {
-            tracing::error!(
-                ?err,
-                json = ?StructuredOutput::Error {
-                    message: format!("{err:?}"),
-                },
-            );
-
-            std::process::exit(1);
-        }
-    };
+async fn process_serve_command() -> Result<()> {
+    let status = tokio::process::Command::new("dx")
+        .arg("serve")
+        .status()
+        .await
+        .context("Failed to spawn dioxus command")?;
+    if !status.success() {
+        anyhow::bail!("Failed to serve Dioxus project.");
+    }
     Ok(())
+}
 
+pub struct BinaryDependencies;
+
+impl BinaryDependencies {
+    pub fn check(dependencies: &[&str]) -> Result<()> {
+        for dependency in dependencies {
+            which::which(dependency).context(format!("The \"{}\" binary is not installed.", dependency))?;
+        }
+        Ok(())
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // If we're being ran as a linker (likely from ourselves), we want to act as a linker instead.
-    if let Some(link_action) = link::LinkAction::from_env() {
-        link_action.run();
-        std::process::exit(0);
-    }
+async fn main() -> Result<()> {
+    BinaryDependencies::check(&["dx"])?;
 
-    let args = Args::parse();
-
-    dioxus_cli::VERBOSITY.set(args.verbosity).expect("Failed to set verbosity");
-
+    let args = Args::try_parse_from(std::env::args().skip(1))?;
     match args.command {
-        Commands::AllCommands(opts) => process_all_commands(opts).await,
-        Commands::WebComponent(opts) => process_all_commands(opts.command).await,
+        Commands::New(opts) => process_new_command(opts).await,
+        Commands::Serve => process_serve_command().await,
     }
 }
